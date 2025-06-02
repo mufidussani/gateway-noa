740b345a45d1528f14a0a7b0336cf42edd83fe39
mufidussani <sanimufidus@gmail.com>
2025-06-02 08:13:45 +0700

    fix: redis host

--- src/configs/redis.ts
+++ src/configs/redis.ts
@@ -1,13 +1,23 @@
 import Redis from "ioredis";
-
 require("dotenv").config();
 
 const HOST = process.env.REDIS_HOST || "";
 const PORT = process.env.REDIS_PORT || 0;
+const baseRoute = process.env.BASE_ROUTE || "";
 
-const redis = new Redis({
-  port: +PORT, // Redis port
-  host: HOST, // Redis host
-});
+let redis: Redis;
+
+// production
+if (baseRoute === "") {
+  redis = new Redis({
+    port: +PORT, // Redis port
+    host: HOST, // Redis host
+  });
+} else {
+  // development
+  redis = new Redis({
+    path: "/home/wasteand/redis.sock",
+  });
+}
 
 export default redis;
